0dc1c73a1e4fd245ca17bbc370c2cea833339232
Dat Nguyen <datntdev@outlook.com>
2025-08-01 23:12:13 +0700

    learn: (auth) initialize project for learning openiddict

---
+++ microsoft/datntdev.OpenIddict/datntdev.OpenIddict.IdentityServer/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace datntdev.OpenIddict.IdentityServer.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
